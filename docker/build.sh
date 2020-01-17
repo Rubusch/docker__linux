cfa78e07418e14b399913e69fe59286892980898
Lothar Rubusch <l.rubusch@gmx.ch>
2020-01-17 15:30:06 +0100

    build.sh: update

--- docker/build.sh
+++ docker/build.sh
@@ -18,7 +18,9 @@ make clean
 #find ${SOURCES} -type f -name \*.pdf -exec cp -arf {} /mnt \;
 
 ## individual docs ( targets from https://www.kernel.org/doc/makehelp.txt )
-## FAILING: driver-api RCU media
+## FAILING: driver-api
+## FAILING: RCU
+## FAILING: media
 for doc in iio hwmon vm openrisc sh PCI power leds livepatch mips arm64 powerpc accounting target virt timers x86 networking input mic cdrom infiniband core-api w1 pcmcia netlabel riscv kbuild userspace-api watchdog s390 filesystems fb dev-tools kernel-hacking arm translations scheduler firmware-guide isdn admin-guide doc-guide block locking spi usb maintainer sound process sparc bpf security xtensa misc-devices fault-injection m68k parisc ia64 i2c gpu fpga trace hid ide crypto; do
     echo
     echo "*** ${doc} *******************************************************************************"
