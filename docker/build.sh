470ac147ef722ba6832d7666ef10d550452312f0
Lothar Rubusch <l.rubusch@gmx.ch>
2020-01-17 15:03:42 +0100

    build.sh: by default build documentation, as far as it builds

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,21 @@ done
 
 cd ${SOURCES}
 make clean
-make-kpkg -j16 kernel-image modules-image kernel-headers --initrd
 
-cp -arvf /home/$(whoami)/*.deb /mnt
+## build kernel
+#make-kpkg -j16 kernel-image modules-image kernel-headers --initrd
+#cp -arvf /home/$(whoami)/*.deb /mnt
+
+## build documentation
+#make pdfdocs
+#find ${SOURCES} -type f -name \*.pdf -exec cp -arf {} /mnt \;
+
+## individual docs ( targets from https://www.kernel.org/doc/makehelp.txt )
+## FAILING: driver-api RCU
+for doc in iio hwmon vm openrisc sh PCI power leds livepatch mips arm64 powerpc accounting target virt timers x86 networking input mic cdrom infiniband media core-api w1 pcmcia netlabel riscv kbuild userspace-api watchdog s390 filesystems fb dev-tools kernel-hacking arm translations scheduler firmware-guide isdn admin-guide doc-guide block locking spi usb maintainer sound process sparc bpf security xtensa misc-devices fault-injection m68k parisc ia64 i2c gpu fpga trace hid ide crypto; do
+    echo
+    echo "*** ${doc} *******************************************************************************"
+    echo
+    make SPHINXDIRS="${doc}" pdfdocs
+    find ${SOURCES} -type f -name \*.pdf -exec mv {} /mnt \;
+done
