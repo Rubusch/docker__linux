441cbbe373703697265439ac9961af533e6c0101
Lothar Rubusch <l.rubusch@gmx.ch>
2020-02-18 16:49:03 +0100

    build.sh: compilation as user update

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
-KDEFCONFIG=x86_64_thinkpad-x1_6th-gen_20kh-ct01ww_defconfig
+export KDEFCONFIG=x86_64_thinkpad-x1_6th-gen_20kh-ct01ww_defconfig
+export MY_HOME="/home/$(whoami)"
 
 cd linux
 make clean
 make $KDEFCONFIG
 make-kpkg -j16 kernel-image modules-image kernel-headers --initrd
 
-## obtain build artifacts
-cp -arvf /root/*.deb /mnt/
+sudo chown $(whoami).$(whoami) -R ${MY_HOME}/output
+cp -arvf ${MY_HOME}/*.deb ${MY_HOME}/output/
