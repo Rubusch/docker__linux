e145837d6011c40423c44e1408afc9d7149fe9be
Lothar Rubusch <l.rubusch@gmx.ch>
2020-02-18 15:53:56 +0100

    build.sh: update defconfig handling

--- docker/build.sh
+++ docker/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
-##
-## started with:
-## $ docker run -ti -v $PWD/mnt:$(pwd)/output afb489d932bc
-##
-KDEFCONFIG=thinkpad-x1_6th-gen_20kh-ct01ww_defconfig
+KDEFCONFIG=x86_64_thinkpad-x1_6th-gen_20kh-ct01ww_defconfig
 
-cp -arf ./linux-defconfigs/arch/x86_64/configs/$KDEFCONFIG ./linux/.config
 cd linux
 make clean
+make $KDEFCONFIG
 make-kpkg -j16 kernel-image modules-image kernel-headers --initrd
 
 ## obtain build artifacts
