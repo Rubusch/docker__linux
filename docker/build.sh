bb46eeb76a8d734a4a84f04246456d0e501eb347
Lothar Rubusch <l.rubusch@gmx.ch>
2020-03-13 17:11:03 +0100

    build.sh: update

--- docker/build.sh
+++ docker/build.sh
@@ -6,8 +6,8 @@ for item in ${SHARES[*]}; do
     test -e ${item} && sudo chown $(whoami).$(whoami) -R ${item}
 done
 
-cd ${SOURCES}
-make clean
+#cd ${SOURCES}
+#make clean
 
 ## build kernel
 #make-kpkg -j16 kernel-image modules-image kernel-headers --initrd
@@ -21,10 +21,14 @@ make clean
 ## FAILING: driver-api
 ## FAILING: RCU
 ## FAILING: media
-for doc in iio hwmon vm openrisc sh PCI power leds livepatch mips arm64 powerpc accounting target virt timers x86 networking input mic cdrom infiniband core-api w1 pcmcia netlabel riscv kbuild userspace-api watchdog s390 filesystems fb dev-tools kernel-hacking arm translations scheduler firmware-guide isdn admin-guide doc-guide block locking spi usb maintainer sound process sparc bpf security xtensa misc-devices fault-injection m68k parisc ia64 i2c gpu fpga trace hid ide crypto; do
-    echo
-    echo "*** ${doc} *******************************************************************************"
-    echo
-    make SPHINXDIRS="${doc}" pdfdocs
-    find ${SOURCES} -type f -name \*.pdf -exec mv {} /mnt \;
-done
+#for doc in iio hwmon vm openrisc sh PCI power leds livepatch mips arm64 powerpc accounting target virt timers x86 networking input mic cdrom infiniband core-api w1 pcmcia netlabel riscv kbuild userspace-api watchdog s390 filesystems fb dev-tools kernel-hacking arm translations scheduler firmware-guide isdn admin-guide doc-guide block locking spi usb maintainer sound process sparc bpf security xtensa misc-devices fault-injection m68k parisc ia64 i2c gpu fpga trace hid ide crypto; do
+#    echo
+#    echo "*** ${doc} *******************************************************************************"
+#    echo
+#    make SPHINXDIRS="${doc}" pdfdocs
+#    find ${SOURCES} -type f -name \*.pdf -exec mv {} /mnt \;
+#done
+echo "Nothing was performed!"
+echo "Please login to this container appending '/bin/bash' to the docker command"
+echo "READY."
+echo
