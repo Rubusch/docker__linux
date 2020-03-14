215bf3632f02787d94c9de4c5ce43ea0bfc74f70
Lothar Rubusch <l.rubusch@gmx.ch>
2020-03-14 10:14:32 +0100

    Kernelpatch setup in shared space

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Docker: linux
 
 
-A docker image/container description for kernel development  
+A docker image for kernel patch development. A ``build.sh`` script will in a second step clone a staging tree. ``configs`` and ``staging`` tree will be setup outside the container, but shared with the container.  
 
 
 ## Build
@@ -13,7 +13,7 @@ $ time docker build --no-cache --build-arg USER=$USER -t rubuschl/linux:$(date +
     10m...
 ```
 
-## Usage
+## Sources
 
 Obtain the tag number from docker images as below  
 
@@ -23,10 +23,19 @@ $ docker images
     rubuschl/linux           20191203212934      70dce0bd5619        15 minutes ago      612MB
 ```
 
+Obtain kernel sources or update
+
+```
+$ docker run --rm -ti --user=$USER:$USER --workdir=/home/$USER -v $PWD/configs:/home/$USER/configs -v $PWD/linux:/home/$USER/linux rubuschl/linux:20191203212934
+```
+
+
+## Usage
+
 Login into the docker container  
 
 ```
-$ docker run --rm -ti --user=$USER:$USER --workdir=/home/$USER -v $PWD/output:/home/$USER/output rubuschl/linux:20191203212934 /bin/bash
+$ docker run --rm -ti --user=$USER:$USER --workdir=/home/$USER -v $PWD/configs:/home/$USER/configs -v $PWD/linux:/home/$USER/linux rubuschl/linux:20191203212934 /bin/bash
 ```
 
 Obtain the current config as a starting point  
--- docker/build.sh
+++ docker/build.sh
@@ -1,34 +1,27 @@
 #!/bin/bash -e
-SOURCES="/home/$(whoami)/linux"
+HOME="/home/$(whoami)"
+BRANCH="master"
+SOURCES="${HOME}/linux"
+CONFIGS="${HOME}/configs"
+SHARES=( ${SOURCES} ${CONFIGS} )
 
-SHARES=( /mnt )
+## fix permissions
 for item in ${SHARES[*]}; do
     test -e ${item} && sudo chown $(whoami).$(whoami) -R ${item}
 done
 
-#cd ${SOURCES}
-#make clean
+cd ${HOME}
 
-## build kernel
-#make-kpkg -j16 kernel-image modules-image kernel-headers --initrd
-#cp -arvf /home/$(whoami)/*.deb /mnt
+if [[ -d "${SOURCES}/.git" ]]; then
+  cd linux
+  git fetch --all origin
+else
+  #git clone -j4 --depth=1 --branch $BRANCH https://github.com/Rubusch/linux.git linux
+  git clone -j4 --depth=1 --branch staging-testing git://git.kernel.org/pub/scm/linux/kernel/git/gregkh/staging.git linux
+fi
 
-## build documentation
-#make pdfdocs
-#find ${SOURCES} -type f -name \*.pdf -exec cp -arf {} /mnt \;
+cd ${SOURCES}
+make clean
 
-## individual docs ( targets from https://www.kernel.org/doc/makehelp.txt )
-## FAILING: driver-api
-## FAILING: RCU
-## FAILING: media
-#for doc in iio hwmon vm openrisc sh PCI power leds livepatch mips arm64 powerpc accounting target virt timers x86 networking input mic cdrom infiniband core-api w1 pcmcia netlabel riscv kbuild userspace-api watchdog s390 filesystems fb dev-tools kernel-hacking arm translations scheduler firmware-guide isdn admin-guide doc-guide block locking spi usb maintainer sound process sparc bpf security xtensa misc-devices fault-injection m68k parisc ia64 i2c gpu fpga trace hid ide crypto; do
-#    echo
-#    echo "*** ${doc} *******************************************************************************"
-#    echo
-#    make SPHINXDIRS="${doc}" pdfdocs
-#    find ${SOURCES} -type f -name \*.pdf -exec mv {} /mnt \;
-#done
-echo "Nothing was performed!"
-echo "Please login to this container appending '/bin/bash' to the docker command"
 echo "READY."
 echo
