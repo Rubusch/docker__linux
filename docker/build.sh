e5280dd97f604dc245a9f0c6c89ef2f3a89c66a2
Lothar Rubusch <l.rubusch@gmx.ch>
2020-03-15 17:34:19 +0100

    Dockerfile: fix codespell, email, etc

--- docker/build.sh
+++ docker/build.sh
@@ -12,13 +12,25 @@ done
 
 cd ${HOME}
 
+## get sources
 if [[ -d "${SOURCES}/.git" ]]; then
-  cd linux
+  cd ${SOURCES}
   git fetch --all
 else
   #git clone -j4 --branch $BRANCH https://github.com/Rubusch/linux.git linux
   git clone -j4 --branch staging-testing git://git.kernel.org/pub/scm/linux/kernel/git/gregkh/staging.git linux
+  #git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 fi
+
+## generate TAGS
+cd ${SOURCES}
+rm -f ./TAGS
+find . -regex ".*\.\(h\|c\)$" -exec etags -a {} \;
+
+## setup checkpatch / codespell
+echo "#!/bin/sh" > ${SOURCES}/.git/hooks/post-commit
+echo "exec git show --format=email HEAD | ./scripts/checkpatch.pl --strict --codespell" >> ${SOURCES}/.git/hooks/post-commit
+chmod a+x ${SOURCES}/.git/hooks/post-commit
 
 cd ${SOURCES}
 make clean
