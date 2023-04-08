f5790eddc4afc54e1debde060853799feb60cdde
Lothar Rubusch <l.rubusch@gmail.com>
2023-04-08 17:31:57 +0200

    scripts: build kernel71 (rpi, 32 bit)
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker/build_context/90_devel-preparation.sh
+++ docker/build_context/90_devel-preparation.sh
@@ -1,20 +1,41 @@
 #!/bin/sh -e
 ##
 ## development tools setup
-##
+## ref: https://www.raspberrypi.com/documentation/computers/linux_kernel.html
 SOURCES_DIR="${1}"
 if [ ! -d "${SOURCES_DIR}" ]; then
     echo "FAIL: no linux sources available in '${SOURCES_DIR}'"
     exit 1
 fi
+cd "${SOURCES_DIR}"
+
+## source toolchain
+source ${WORKSPACE_DIR}/source-me.sh
+
+## prepare sources
+make bcm2709_defconfig
+
+## build kernel
+make -j4 zImage modules dtbs
+
+## make debian package with kernel and modules (will be in upper directory)
+make bindeb-pkg
 
 ## generate TAGS
-test -d "${SOURCES_DIR}" || exit 1
-cd "${SOURCES_DIR}"
 rm -f ./TAGS
-find . -regex ".*\.\(h\|c\)$" -exec etags -a {} \;
+make tags
 
 ## setup checkpatch / codespell
 echo "#!/bin/sh" > "${SOURCES_DIR}/.git/hooks/post-commit"
 echo "exec git show --format=email HEAD | ./scripts/checkpatch.pl --strict --codespell" >> "${SOURCES_DIR}/.git/hooks/post-commit"
 chmod a+x "${SOURCES_DIR}/.git/hooks/post-commit"
+
+
+## manual installation then follows (on the target)
+##
+#make -j4 zImage modules dtbs
+#sudo make modules_install
+#sudo cp arch/arm/boot/dts/*.dtb /boot/
+#sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+#sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
+#sudo cp arch/arm/boot/zImage /boot/$KERNEL.img
