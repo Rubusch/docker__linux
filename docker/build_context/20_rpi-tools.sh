5f4880f0072f20186077d1ee8cae25b39a22a7e7
Lothar Rubusch <l.rubusch@gmail.com>
2023-04-08 17:25:19 +0200

    scripts: generate a new .env file
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker/build_context/20_rpi-tools.sh
+++ docker/build_context/20_rpi-tools.sh
@@ -6,4 +6,5 @@ cd "${MY_HOME}"
 if [ ! -e "${WORKSPACE_DIR}/source-me.sh" ]; then
     echo 'export CROSS_COMPILE=arm-linux-gnu-' > ${WORKSPACE_DIR}/source-me.sh
     echo 'export ARCH=arm' >> ${WORKSPACE_DIR}/source-me.sh
+    echo 'export KERNEL=kernel7' >> ${WORKSPACE_DIR}/source-me.sh
 fi
