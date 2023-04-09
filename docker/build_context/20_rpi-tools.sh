930d743ae6f47bb8ae1fe490ea164d269efb7ebd
Lothar Rubusch <l.rubusch@gmail.com>
2023-04-09 09:29:10 +0200

    scripts: fix toolchain prefix
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker/build_context/20_rpi-tools.sh
+++ docker/build_context/20_rpi-tools.sh
@@ -4,7 +4,7 @@
 ##
 cd "${MY_HOME}"
 if [ ! -e "${WORKSPACE_DIR}/source-me.sh" ]; then
-    echo 'export CROSS_COMPILE=arm-linux-gnu-' > ${WORKSPACE_DIR}/source-me.sh
+    echo 'export CROSS_COMPILE=arm-linux-gnueabihf-' > ${WORKSPACE_DIR}/source-me.sh
     echo 'export ARCH=arm' >> ${WORKSPACE_DIR}/source-me.sh
     echo 'export KERNEL=kernel7' >> ${WORKSPACE_DIR}/source-me.sh
 fi
