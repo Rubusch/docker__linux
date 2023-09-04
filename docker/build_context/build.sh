c13a1d5019bd2e78f69bac6f299ea25faba310e4
Lothar Rubusch <l.rubusch@gmail.com>
2023-09-04 11:59:56 +0200

    scripts: fix machine config not found error
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker/build_context/build.sh
+++ docker/build_context/build.sh
@@ -8,6 +8,52 @@ export CONFIGS_DIR="${MY_HOME}/configs"
 ## prepare
 00_devenv.sh "${WORKSPACE_DIR}" "${CONFIGS_DIR}"
 
+## prepare machine config, in case source the file
+if [ ! -e "${WORKSPACE_DIR}/machine.conf" ]; then
+     cat<<EOF > "${WORKSPACE_DIR}/machine.conf"
+## rpi4 (64)
+## ref: https://www.raspberrypi.com/documentation/computers/linux_kernel.html
+export LINUX_BRANCH="rpi-6.3.y"
+export CROSS_COMPILE="aarch64-linux-gnu-"
+export ARCH="arm64"
+export KERNEL="kernel8"
+export KDEFCONFIG_NAME="bcm2711_defconfig"
+export KERNEL_URL="https://github.com/raspberrypi/linux"
+
+## rpi3b (32)
+#export LINUX_BRANCH="rpi-6.3.y"
+#export CROSS_COMPILE="arm-linux-gnueabihf-"
+#export ARCH="arm"
+#export KERNEL="kernel7"
+#export KDEFCONFIG_NAME="bcm2709_defconfig"
+
+## linux kernel staging (patches) - gkh
+#export LINUX_BRANCH=TODO
+#export CROSS_COMPILE=TODO
+#export ARCH=TODO
+#export KERNEL=TODO
+#export KDEFCONFIG_NAME=TODO
+#export KERNEL_URL="git://git.kernel.org/pub/scm/linux/kernel/git/gregkh/staging.git"
+
+## linux kernel staging (patches) - thorvalds
+#export LINUX_BRANCH=TODO
+#export CROSS_COMPILE=TODO
+#export ARCH=TODO
+#export KERNEL=TODO
+#export KDEFCONFIG_NAME=TODO
+#export KERNEL_URL="git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
+
+## linux kernel next (testing) - kernel.org
+#export LINUX_BRANCH=TODO
+#export CROSS_COMPILE=TODO
+#export ARCH=TODO
+#export KERNEL=TODO
+#export KDEFCONFIG_NAME=TODO
+#export KERNEL_URL="git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git"
+EOF
+fi
+source "${WORKSPACE_DIR}/machine.conf"
+
 ## get sources
 10_linux-devel-sources.sh "${SOURCES_DIR}"
 
