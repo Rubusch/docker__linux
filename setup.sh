55713563e91506fd6b2f56f8ca210c6587243e1d
Lothar Rubusch <l.rubusch@gmail.com>
2024-03-26 21:25:27 +0100

    docker: update (WIP)
    - avoid using docker-compose
    - build was replaced by buildx, adjustments ongoing
    
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -12,21 +12,19 @@ test -d "docker" || die "wrong directory, execute script from top level"
 
 cd ./docker
 
+if [ ! -z "$(docker images -q ${USER}/linux-develop)" ]; then
+	docker build \
+		--build-arg UID=$(id -u) \
+		--build-arg GID=$(id -g) \
+		--build-context context=./build_context \
+		-t ${USER}/linux-develop \
+		./build_context
+	exit 0
+fi
+
 if [ ! -f .env ]; then
     echo "UID=$(id -u)" > .env
     echo "GID=$(id -g)" >> .env
-fi
-
-## defaults to script build.sh if LOGIN_SHELL is empty and no image around
-## executes tool, if provided as prefixed LOGIN_SHELL variable, e.g.
-##
-## $ LOGIN_SHELL="cat /etc/issue" ./setup.sh
-
-if [ -z "$LOGIN_SHELL" ]; then
-    if [ ! -z "$(docker images -q ${USER}/linux-develop)" ]; then
-	## login into shell
-	LOGIN_SHELL="/bin/bash"
-    fi
 fi
 
 echo "now source the environment:"
@@ -44,4 +42,5 @@ docker run \
 	--mount type=bind,source=$(pwd)/build_configs,target=/home/$USER/configs \
 	-v ./workspace:/home/$USER/workspace \
 	${USER}/linux-develop \
-	${LOGIN_SHELL}
+	/bin/bash
+
